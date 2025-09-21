08dc83d8df73c94e4b479d4fb87c781ea219dd15
esraaabuhalawa <esr.saad123@gmail.com>
2025-09-21 20:16:05 +0300

    update

--- src/app/features/categories/categories.component.ts
+++ src/app/features/categories/categories.component.ts
@@ -3,23 +3,37 @@ import { Category } from '../../core/models/category.interface';
 import { CategoriesService } from '../../core/services/categories/categories.service';
 import { Subscription } from 'rxjs';
 import { RouterLink } from '@angular/router';
+import { LoaderComponent } from "../../shared/components/loader/loader.component";
 @Component({
   selector: 'app-categories',
-  imports: [ RouterLink ],
+  imports: [RouterLink, LoaderComponent],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent implements OnInit, OnDestroy {
   categories: Category[] = [];
   categoriesSubscribe: Subscription | null = null;
+  isLoading:Boolean = false;
+
   private readonly categoryService = inject(CategoriesService);
 
-  ngOnInit(): void {
+  getCategories(){
+    this.isLoading = true;
     this.categoriesSubscribe = this.categoryService.getAllCategories().subscribe({
-      next: (res) => this.categories = res.data || [],
-      error: (err) => console.log(err)
+      next: (res) => {
+        this.isLoading = false;
+        this.categories = res.data || []
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.log(err)
+      }
     });
   }
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
   ngOnDestroy(): void {
     if (this.categoriesSubscribe) {
       this.categoriesSubscribe.unsubscribe();
