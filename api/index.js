9f1f8f1161deadd4376f5ec908474ac555661fb9
esraaabuhalawa <esr.saad123@gmail.com>
2026-01-02 23:22:16 +0200

    update

--- api/index.js
+++
@@ -1,4 +0,0 @@
-import { createRequestHandler } from '@angular/ssr';
-import serverModule from '../dist/E-commerce-project/server/server.mjs';
-
-export default createRequestHandler(serverModule.default);
