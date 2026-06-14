ee76ca2012e6711180bae256b0f4fc3429811f9d
esraaabuhalawa <esr.saad123@gmail.com>
2026-06-14 23:16:10 +0300

    Update fonts and solve carousel error

--- src/app/shared/components/similar-products/similar-products.component.ts
+++ src/app/shared/components/similar-products/similar-products.component.ts
@@ -5,19 +5,68 @@ import { Subscription } from 'rxjs';
 import { ProductCardComponent } from "../product-card/product-card.component";
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-similar-products',
-  imports: [ProductCardComponent,CarouselModule,TranslateModule],
+  imports: [ProductCardComponent, CarouselModule, TranslateModule],
   templateUrl: './similar-products.component.html',
   styleUrl: './similar-products.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SimilarProductsComponent implements OnInit {
-  @Input() id: string = ''
-  private readonly productService = inject(ProductsService);
   productsList: Product[] = [];
   getProducts: Subscription | null = null;
+  customOptions!: OwlOptions;
+
+  @Input() id: string = ''
+  private readonly productService = inject(ProductsService);
+
+  constructor(private translate: TranslateService) {
+    this.setCarouselOptions(this.translate.currentLang);
+
+    //when langauge change
+    this.translate.onLangChange.subscribe(event => {
+      this.setCarouselOptions(event.lang);
+    });
+  }
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  setCarouselOptions(lang: string) {
+    const isRTL = lang === 'ar';
+    this.customOptions = {
+      loop: true,                     // infinite looping
+      autoplay: true,                 // enable auto play
+      autoplayTimeout: 6000,          // 3s per slide
+      autoplayHoverPause: true,
+      smartSpeed: 1000,                 //  smooth transition speed
+      animateOut: 'fadeOut',           // optional smooth fade
+      mouseDrag: true,
+      rtl: isRTL,
+      touchDrag: true,
+      pullDrag: true,
+      dots: false,
+      margin: 25,                      // margin between items
+      navSpeed: 700,
+      responsive: {
+        0: {
+          items: 1
+        },
+        400: {
+          items: 2
+        },
+        740: {
+          items: 3
+        },
+        940: {
+          items: 4
+        },
+      },
+      nav: false
+    }
+  }
 
   fetchProducts() {
     const filters: any = {};
@@ -37,39 +86,6 @@ export class SimilarProductsComponent implements OnInit {
     });
   }
 
-    customOptions: OwlOptions = {
-    loop: true,                     // infinite looping
-    autoplay: true,                 // enable auto play
-    autoplayTimeout: 6000,          // 3s per slide
-    autoplayHoverPause: true,
-    smartSpeed: 1000,                 // 👈 smooth transition speed
-    animateOut: 'fadeOut',           // 👈 optional smooth fade
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: false,
-    margin: 25,                      // 👈 margin between items
-    navSpeed: 700,
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      },
-    },
-    nav: false
-  }
-
-  ngOnInit(): void {
-    this.fetchProducts();
-  }
   ngOnDestroy(): void {
     if (this.getProducts) {
       this.getProducts.unsubscribe();
